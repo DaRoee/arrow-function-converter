e20a8cab159107b05760fe7f712f3d88b5549538
Drucker <roeexd@gmail.com>
2018-12-01 16:35:40 +0200

    Correct indentation

--- src/extension.ts
+++ src/extension.ts
@@ -32,14 +32,15 @@ function activate(context: any) {
       }
 
       const actualContent = content.split(/\).$/)[0];
-      const contentCol = editor.options.tabSize;
+      const tabSize = editor.options.tabSize;
+      const startCol = "\t".repeat(Math.floor(col / tabSize));
       params = verifyParams(params);
       const newHeader = `${header}(${params} => {\n\n`;
-      const newContent = `${" ".repeat(contentCol)}return ${actualContent.trim()}\n`;
-      const footer = `${" ".repeat(col)}});`;
+      const newContent = `${startCol}\treturn ${actualContent.trim()};\n`;
+      const footer = `${startCol}});`;
 
       editor.edit((editBuilder: any) => {
-        editBuilder.replace( new vscode.Range(line, 0, line, 9999), newHeader + newContent + footer);
+        editBuilder.replace(new vscode.Range(line, 0, line, 9999), newHeader + newContent + footer);
       }).then(() => {
         // selection.active.character = contentCol;
         // selection.active.line = line + 1;
